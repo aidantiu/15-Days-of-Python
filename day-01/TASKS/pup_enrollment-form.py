891c403482d55dfa8fc2e5da1bafdb9eccf1ff75
Aidan Tiu <147817732+aidantiu@users.noreply.github.com>
2024-01-12 14:39:56 +0800

    Update pup_enrollment-form.py

--- day-01/TASKS/pup_enrollment-form.py
+++ day-01/TASKS/pup_enrollment-form.py
@@ -1,14 +1,19 @@
 # Write the code ↓ to read user's input.
 # Be cautious when reading input of various data types.
-
-
-
-
-
+print("PUP Enrollment Form\n")
+fullName = str(input("Enter your full name: "))
+age = int(input("Enter your age: "))
+gwa = float(input("Enter your previous general weighted average: "))
+isMember = bool(input("Are you a member of AWS Cloud Club? (yes/no): ").lower() == "yes")
 
 # Write the code ↓ to display the user's personal information.
-# Select and employ a string concatenation method based on your personal preference and comfort level.
+print("\nYour Enrollment Form:")
 
+# Select and employ a string concatenation method based on your personal preference and comfort level.
+print(f"Name: {fullName}")
+print(f"Age: {age}")
+print(f"GWA: {gwa}")
+print(f"Awstraunot: {isMember}")
 
 
 
