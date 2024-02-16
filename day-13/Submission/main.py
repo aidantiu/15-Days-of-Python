b7ba45b7acde7d1e1807e20bac411a1edda59c04
Aidan Tiu <147817732+aidantiu@users.noreply.github.com>
2024-02-16 13:43:43 +0800

    Update main.py and add submission req. files

--- day-13/Submission/main.py
+++ day-13/Submission/main.py
@@ -1,15 +1,20 @@
-from flask import Flask, jsonify  
+from flask import Flask, jsonify
+import csv
 
 app = Flask(__name__)
 
 csv_file = 'programming_languages.csv'
 
 def read_csv():
-    pass
+    with open(csv_file, 'r', encoding='utf-8') as file:
+        csv_reader = csv.DictReader(file)
+        data = [row for row in csv_reader]
+    return data
 
 @app.route('/', methods=['GET'])
 def get_langs():
-    pass
+    data = read_csv()
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
